a47956f64f6b5fa2238ffabad0f7bb301767c0c4
HMPhuoc <huynhminhphuoc01@gmail.com>
2023-04-28 23:57:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>SQL folder contains .sql file to create table used for ETL processes.</h1>
+    finalDimFact: creating dimensions and fact table.
+    finalRawData: creating a raw data table for the ETL processes.
+
+
+<h1>Data folder contains .xls file that contains data of the project.</h1>
+    HDHI Admission data: data of hospital admission.
+    HDMI Mortality data: data of death case.
+    table_heading: name of colums and explaination.
+
+<h1>FinalProjectETL contains the ETL project.</h1>
---
+++ SQL/finalDimFact.sql
@@ -0,0 +1,90 @@
+USE [FinalProject]
+GO
+/****** Object:  Table [dbo].[AnamDim]    Script Date: 25-Apr-23 5:23:28 PM ******/
+CREATE TABLE [dbo].[AnaemiaDim](
+	[RAISED CARDIAC ENZYMES] [bit] NULL,
+	[SEVERE ANAEMIA] [bit] NULL,
+	[ANAEMIA] [bit] NULL,
+	[DM] [bit] NULL,
+	[CAD] [bit] NULL,
+	[HTN] [bit] NULL,
+	[PRIOR CMP] [bit] NULL,
+	[CKD] [bit] NULL,
+	[AmneID] [int] primary key,
+)
+GO
+/****** Object:  Table [dbo].[DiagDim]    Script Date: 25-Apr-23 5:23:28 PM ******/
+
+CREATE TABLE [dbo].[DiagnosisDim](
+	[STABLE ANGINA] [bit] NULL,
+	[ACS] [bit] NULL,
+	[STEMI] [bit] NULL,
+	[ATYPICAL CHEST PAIN] [bit] NULL,
+	[HEART FAILURE] [bit] NULL,
+	[HFREF] [bit] NULL,
+	[HFNEF] [bit] NULL,
+	[VALVULAR] [bit] NULL,
+	[CHB] [bit] NULL,
+	[SSS] [bit] NULL,
+	[AKI] [bit] NULL,
+	[CVA INFRACT] [bit] NULL,
+	[CVA BLEED] [bit] NULL,
+	[AF] [bit] NULL,
+	[VT] [bit] NULL,
+	[PSVT] [bit] NULL,
+	[CONGENITAL] [bit] NULL,
+	[UTI] [bit] NULL,
+	[NEURO CARDIOGENIC SYNCOPE] [bit] NULL,
+	[ORTHOSTATIC] [bit] NULL,
+	[INFECTIVE ENDOCARDITIS] [bit] NULL,
+	[DVT] [bit] NULL,
+	[CARDIOGENIC SHOCK] [bit] NULL,
+	[SHOCK] [bit] NULL,
+	[PULMONARY EMBOLISM] [bit] NULL,
+	[CHEST INFECTION] [bit] NULL,
+	[DiagID] [int] primary key,
+)
+GO
+/****** Object:  Table [dbo].[HabbitDim]    Script Date: 25-Apr-23 5:23:28 PM ******/
+
+CREATE TABLE [dbo].[HabbitDim](
+	[HabbitID] [int] primary key,
+	[HabbitName] [varchar](50) NULL,
+	[SMOKING] [bit] NULL,
+	[ALCOHO] [bit] NULL
+)
+GO
+/****** Object:  Table [dbo].[PersonalHealthDim]    Script Date: 25-Apr-23 5:23:28 PM ******/
+CREATE TABLE [dbo].[PersonalHealthDim](
+	[HB] decimal(7,2) NULL,
+	[TLC] decimal(7,2) NULL,
+	[PLATELETS] decimal(7,2) NULL,
+	[GLUCOSE] decimal(7,2) NULL,
+	[UREA] decimal(7,2) NULL,
+	[CREATININE] decimal(7,2) NULL,
+	[BNP] integer NULL,
+	[EF] integer NULL,
+	[HealthID] [int] NOT NULL,
+)
+GO
+/****** Object:  Table [dbo].[PersonDim]    Script Date: 25-Apr-23 5:23:28 PM ******/
+CREATE TABLE [dbo].[PersonDim](
+	[AGE] [int] NULL,
+	[GENDER] [nvarchar](255) NULL,
+	[RURAL] [nvarchar](255) NULL,
+	[PesonID] [int] primary key,
+)
+GO
+/****** Object:  Table [dbo].[TimeDim]    Script Date: 25-Apr-23 5:23:28 PM ******/
+
+CREATE TABLE [dbo].[TimeDim](
+	[DischargeDay] [int] NULL,
+	[DischargeMonth] [int] NULL,
+	[DischargeQuarter] [int] NULL,
+	[DischargeYear] [int] NULL,
+	[AdmissionDay] [int] NULL,
+	[AdmissionMonth] [int] NULL,
+	[AdmissionQuarter] [int] NULL,
+	[AdmissionYear] [int] NULL,
+	[TimeID] [int] primary key,
+)
